276a632b2074afbdf429fc7c6a13bc2be6102c5c
Kristoffer Richardsson <kristoffer@bitcraze.io>
2016-05-12 17:04:48 +0200

    Added UART production test

--- src/production_test.c
+++ src/production_test.c
@@ -5,15 +5,12 @@
 #include "test_support.h"
 
 static void initUart(UART_HandleTypeDef* huart, USART_TypeDef* instance);
-static bool runUartLoopbackTest();
+static void initPins(UART_HandleTypeDef* huart);
 
-static UART_HandleTypeDef uart1;
-static UART_HandleTypeDef uart3;
+static bool runUartLoopbackTests();
+static bool runUartLoopbackTest(UART_HandleTypeDef* from, UART_HandleTypeDef* to, char* errorMessage);
 
 bool productionTestsRun() {
-  initUart(&uart1, USART1);
-  initUart(&uart3, USART3);
-
   bool result = true;
 
   printf("------------------------------\r\n");
@@ -21,7 +18,7 @@ bool productionTestsRun() {
   printf("\r\n");
 
   testSupportPrintStart("UART production test");
-  testSupportReport(&result, runUartLoopbackTest());
+  testSupportReport(&result, runUartLoopbackTests());
 
   printf("------------------------------\r\n");
   printf("TEST\t: Production tests ");
@@ -31,7 +28,47 @@ bool productionTestsRun() {
 }
 
 
-void initUart(UART_HandleTypeDef* huart, USART_TypeDef* instance) {
+static bool runUartLoopbackTests() {
+  UART_HandleTypeDef uart1;
+  UART_HandleTypeDef uart3;
+
+  initUart(&uart1, USART1);
+  initUart(&uart3, USART3);
+
+  initPins(&uart1);
+  initPins(&uart3);
+
+  return runUartLoopbackTest(&uart1, &uart3, "UART1 to UART3") &&
+         runUartLoopbackTest(&uart3, &uart1, "UART3 to UART1");
+}
+
+static bool runUartLoopbackTest(UART_HandleTypeDef* from, UART_HandleTypeDef* to, char* errorMessage) {
+  uint8_t data = 0;
+  uint8_t received = 0;
+  uint32_t timeout = 1;
+
+  for (data = 0; data < 255; data++) {
+    if (HAL_UART_Transmit(from, &data, 1, timeout) != HAL_OK) {
+      printf(" Failed to write data for %s ", errorMessage);
+      return false;
+    }
+
+    if (HAL_UART_Receive(to, &received, 1, timeout) == HAL_OK) {
+      if (data != received) {
+        printf(" Got unexpected value for %s ", errorMessage);
+        return false;
+      }
+    } else {
+        printf(" Did not get any data for %s ", errorMessage);
+        return false;
+    }
+  }
+
+  return true;
+}
+
+
+static void initUart(UART_HandleTypeDef* huart, USART_TypeDef* instance) {
   huart->Instance = instance;
   huart->Init.BaudRate = 115200;
   huart->Init.WordLength = UART_WORDLENGTH_8B;
@@ -45,14 +82,36 @@ void initUart(UART_HandleTypeDef* huart, USART_TypeDef* instance) {
   HAL_UART_Init(huart);
 }
 
-bool runUartLoopbackTest() {
-  bool result = true;
+void initPins(UART_HandleTypeDef* huart) {
+  GPIO_InitTypeDef GPIO_InitStruct;
 
-  // TODO Add test
+  if(huart->Instance==USART1) {
+    /* Peripheral clock enable */
+    __USART1_CLK_ENABLE();
 
-  if (!result) {
-    printf("TODO report why\r\n");
+    /**USART1 GPIO Configuration
+    PA9     ------> USART1_TX
+    PA10     ------> USART1_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF1_USART1;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  } else if(huart->Instance==USART3) {
+    /* Peripheral clock enable */
+    __USART3_CLK_ENABLE();
+
+    /**USART3 GPIO Configuration
+    PB10     ------> USART3_TX
+    PB11     ------> USART3_RX
+    */
+    GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF4_USART3;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
   }
-
-  return result;
 }
