4617584ee7217a8cf1de16133fc8e4a5c859befb
Arnaud Taffanel <arnaud@bitcraze.se>
2016-05-17 22:53:29 +0200

    Implemented usbcomm layer to print and read the USB serial port
    Implements an init buffer that buffer the init messages and read them
    back when the serial port is opened. This permits to be able to read about
    sensor initialization and other startup messages via USB (the normal
    way would be to reset the board to see the messages again but this is not
    working with USB as it would disconnects the USB serial port on reset).

---
+++ inc/usbcomm.h
@@ -0,0 +1,14 @@
+#ifndef __USB_COMM_H__
+#define __USB_COMM_H
+
+#include <stdbool.h>
+
+int usbcommRead(char* buffer, size_t len);
+
+void usbcommWrite(char const *data, int len);
+
+void usbcommSetSystemStarted(bool init);
+
+void usbcommTick();
+
+#endif // __USB_COMM_H__
---
+++ src/usbcomm.c
@@ -0,0 +1,50 @@
+// USB communication wrapper
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "usbd_conf.h"
+#include "usbd_cdc_if.h"
+
+#define INITBUFFER_LEN 1024
+
+static char initBuffer[INITBUFFER_LEN];
+static int initPtr = 0;
+
+static bool isInit = false;
+
+int usbcommRead(char* buffer, size_t len)
+{
+  return CDC_Read(buffer, len);
+}
+
+void usbcommWrite(char *data, int len)
+{
+  if (isInit) {
+    CDC_WriteBlocking(data, len);
+  } else {
+    for(int i=0; initPtr<INITBUFFER_LEN && i<len; i++,initPtr++) {
+      initBuffer[initPtr] = data[i];
+    }
+  }
+}
+
+void usbcommSetSystemStarted(bool init)
+{
+  isInit = init;
+}
+
+void usbcommTick()
+{
+  static bool wasConnected;
+  CDC_Tick();
+
+  if (isInit) {
+    if (wasConnected != USBD_IsSerialConnected()) {
+      if (USBD_IsSerialConnected()) {
+        CDC_WriteBlocking(initBuffer, initPtr);
+      }
+    }
+    wasConnected = USBD_IsSerialConnected();
+  }
+}
