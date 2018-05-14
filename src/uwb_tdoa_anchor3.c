9a0b9c3c503e8b74cd289fcd8135a5426ed01815
Kristoffer Richardsson <kristoffer@bitcraze.io>
2018-05-14 06:22:11 +0200

    #27 Added dynamic anchor management to TdoA3

--- src/uwb_tdoa_anchor3.c
+++ src/uwb_tdoa_anchor3.c
@@ -7,28 +7,42 @@
  *
  * LPS node firmware.
  *
- * Copyright 2016, Bitcraze AB
+ * Copyright 2018, Bitcraze AB
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * uwb_tdoa_anchor3.c is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with uwb_tdoa_anchor3.c. If not, see <http://www.gnu.org/licenses/>.
  */
 /* uwb_tdoa_anchor3.c: Uwb TDOA anchor V3, randomized tx times and ids */
 
 /*
- * This anchor algorithm is transmitting messages at random times to remove the
- * dependency to anchor 0 in TDoA V2. Ids a also randomized to support dynamic
- * setups with varying number of anchors.
- */
+
+The anchor is part of a static system of other anchors, anchors are not moving.
+New anchors may be added or removed during the lifetime of the system, but this
+type of event is rare and adaptation to a new setup may take a few seconds.
+Packets are transmitted at random times by all anchors and by listening to surrounding
+anchors one anchor can calculate the distance to the other anchors.
+An anchor must not make any assumptions of the ids of surrounding anchors.
+It is assumed that the anchor density is evenly distributed in the covered
+volume and that 5-15 anchors are visible in every point.
+
+The implementation must handle
+1. Transmit at random times
+2. Any anchor ids
+3. Added or removed anchors
+4. Around 20 concurrent visible anchors
+
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -45,10 +59,6 @@
 
 #define debug(...) printf(__VA_ARGS__)
 
-#define NSLOTS 8
-#define TDMA_SLOT_BITS 26 // 26: 2ms timeslot
-#define TDMA_NSLOT_BITS 3
-
 // Time length of the preamble
 #define PREAMBLE_LENGTH_S ( 128 * 1017.63e-9 )
 #define PREAMBLE_LENGTH (uint64_t)( PREAMBLE_LENGTH_S * 499.2e6 * 128 )
@@ -63,36 +73,57 @@
 #define TDMA_HIGH_RES_RAND_S ( 1e-3 )
 #define TDMA_HIGH_RES_RAND (uint64_t)( TDMA_HIGH_RES_RAND_S * 499.2e6 * 128 )
 
-// Timeout for receiving a packet in a timeslot
-// TODO krri What is a good timeout period?
-#define RECEIVE_TIMEOUT 800
+#define ANCHOR_LIST_UPDATE_INTERVAL 1000;
 
-#define TS_TX_SIZE 4
+#define ANCHOR_STORAGE_COUNT 8
+#define REMOTE_TX_MAX_COUNT 8
+#if REMOTE_TX_MAX_COUNT > ANCHOR_STORAGE_COUNT
+  #error "Invalid settings"
+#endif
+
+#define ID_COUNT 256
+#define ID_WITHOUT_CONTEXT 0xff
+#define ID_INVALID 0xff
 
 // Useful constants
 static const uint8_t base_address[] = {0,0,0,0,0,0,0xcf,0xbc};
+
+// Anchor context
+typedef struct {
+  uint8_t id;
+  bool isUsed;
+  uint8_t seqNr;
+  uint32_t rxTimeStamp;
+  uint32_t txTimeStamp;
+  uint16_t distance;
+} anchorContext_t;
 
 // This context struct contains all the required global values of the algorithm
 static struct ctx_s {
   int anchorId;
 
-  // Current packet id and tx timestamps
-  uint8_t pid;
+  // Information about latest transmitted packet
+  uint8_t seqNr;
+  uint32_t txTime; // In UWB clock ticks
 
-  // Next transmit time in ticks
+  // Next transmit time in system clock ticks
   uint32_t nextTxTick;
 
-  // list of timestamps and ids for last frame.
-  uint8_t packetIds[NSLOTS];
-  uint32_t rxTimestamps[NSLOTS];
-  uint32_t txTimestamps[NSLOTS];
+  // List of ids to transmit in remote data section
+  uint8_t remoteTxId[REMOTE_TX_MAX_COUNT];
+  uint8_t remoteTxIdCount;
 
-  uint16_t distances[NSLOTS];
+  // The list of anchors to transmit and store is updated at regular intervals
+  uint32_t nextAnchorListUpdate;
+
+  // Remote anchor data
+  uint8_t anchorCtxLookup[ID_COUNT];
+  anchorContext_t anchorCtx[ANCHOR_STORAGE_COUNT];
+  uint8_t anchorRxCount[ID_COUNT];
 } ctx;
 
 // Packet formats
 #define PACKET_TYPE_TDOA3 0x30
-#define TDOA_3_REMOTE_COUNT_MAX 7
 
 typedef struct {
   uint8_t type;
@@ -120,19 +151,120 @@ typedef struct {
 } __attribute__((packed)) rangePacket3_t;
 
 
-typedef struct {
-  uint8_t type;
-  uint8_t pid[NSLOTS];  // Packet id of the timestamps
-  uint8_t timestamps[NSLOTS][TS_TX_SIZE];  // Relevant time for anchors
-  uint16_t distances[NSLOTS];
-} __attribute__((packed)) rangePacket2_t;
-
-#define FULL_RANGE_PACKET_SIZE (sizeof(rangePacketHeader3_t) + TDOA_3_REMOTE_COUNT_MAX * sizeof(remoteAnchorDataFull_t))
-
-// TODO krri Current implementation always sends TDOA_3_REMOTE_COUNT_MAX remote anchor data
-#define LPP_HEADER FULL_RANGE_PACKET_SIZE
+#define LPP_HEADER 0
 #define LPP_TYPE (LPP_HEADER + 1)
 #define LPP_PAYLOAD (LPP_HEADER + 2)
+
+
+static anchorContext_t* getContext(uint8_t anchorId) {
+  uint8_t slot = ctx.anchorCtxLookup[anchorId];
+
+  if (slot == ID_WITHOUT_CONTEXT) {
+    return 0;
+  }
+
+  return &ctx.anchorCtx[slot];
+}
+
+static void clearAnchorRxCount() {
+  memset(&ctx.anchorRxCount, 0, ID_COUNT);
+}
+
+static void removeAnchorContextsNotInList(const uint8_t* id, const uint8_t count) {
+  for (int i = 0; i < ANCHOR_STORAGE_COUNT; i++) {
+    anchorContext_t* anchorCtx = &ctx.anchorCtx[i];
+    if (anchorCtx->isUsed) {
+      const uint8_t ctxId = anchorCtx->id;
+      bool found = false;
+      for (int j = 0; j < count; j++) {
+        if (id[j] == ctxId) {
+          found = true;
+          break;
+        }
+      }
+
+      if (!found) {
+        ctx.anchorCtxLookup[ctxId] = ID_WITHOUT_CONTEXT;
+        anchorCtx->isUsed = false;
+        debug("-ctx %i %i\r\n", ctxId, i);
+      }
+    }
+  }
+}
+
+static void createAnchorContext(const uint8_t id) {
+  if (ctx.anchorCtxLookup[id] != ID_WITHOUT_CONTEXT) {
+    // Already has a contex, we're done
+    return;
+  }
+
+  for (uint8_t i = 0; i < ANCHOR_STORAGE_COUNT; i++) {
+    anchorContext_t* anchorCtx = &ctx.anchorCtx[i];
+    if (!anchorCtx->isUsed) {
+      ctx.anchorCtxLookup[id] = i;
+
+      memset(anchorCtx, 0, sizeof(anchorContext_t));
+      anchorCtx->id = id;
+      anchorCtx->isUsed = true;
+
+      debug("+ctx %i %i\r\n", id, i);
+      break;
+    }
+  }
+}
+
+static void createAnchorContextsInList(const uint8_t* id, const uint8_t count) {
+  for (uint8_t i = 0; i < count; i++) {
+    createAnchorContext(id[i]);
+  }
+}
+
+
+// This function is called at regular intervals to update lists containing data
+// about which anchors to store and add to outgoing messages. This
+// update might take some time but this should not be a problem since the TX
+// times are randomized anyway. The intention is that we could plug in clever
+// algorithms here that optimizes which anchors to use.
+static void updateAnchorLists() {
+  // First naive approach is to use the first anchors that we have seen since
+  // last update.
+
+  // TODO krri Do something clever
+
+  static uint8_t id[ANCHOR_STORAGE_COUNT];
+  static uint8_t count[ANCHOR_STORAGE_COUNT];
+
+  memset(id, 0, sizeof(id));
+  memset(count, 0, sizeof(count));
+
+  uint8_t index = 0;
+
+  for (int anchor = 0; anchor < ID_COUNT; anchor++) {
+    uint8_t rxCount = ctx.anchorRxCount[anchor];
+
+    if (rxCount > 0) {
+      count[index] = rxCount;
+      id[index++] = anchor;
+
+      if (index >= ANCHOR_STORAGE_COUNT) {
+        break;
+      }
+    }
+  }
+
+  removeAnchorContextsNotInList(id, index);
+  createAnchorContextsInList(id, index);
+
+  uint8_t remoteTXIdIndex = 0;
+  for (int i = 0; i < index; i++) {
+    if (remoteTXIdIndex < REMOTE_TX_MAX_COUNT) {
+      ctx.remoteTxId[remoteTXIdIndex++] = id[i];
+    }
+  }
+  ctx.remoteTxIdCount = remoteTXIdIndex;
+
+  clearAnchorRxCount();
+}
 
 /* Adjust time for schedule transfer by DW1000 radio. Set 9 LSB to 0 */
 static uint32_t adjustTxRxTime(dwTime_t *time)
@@ -144,7 +276,7 @@ static uint32_t adjustTxRxTime(dwTime_t *time)
   return added;
 }
 
-static dwTime_t transmitTimeAsSoonAsPossible(dwDevice_t *dev)
+static dwTime_t findTransmitTimeAsSoonAsPossible(dwDevice_t *dev)
 {
   dwTime_t transmitTime = { .full = 0 };
   dwGetSystemTimestamp(dev, &transmitTime);
@@ -159,9 +291,9 @@ static dwTime_t transmitTimeAsSoonAsPossible(dwDevice_t *dev)
   // TODO krri Adding randomization on this level adds a long delay, is it worth it?
   // The randomization on OS level is quantized to 1 ms (tick time of the system)
   // Add a high res random to smooth it out
-//  uint32_t r = random();
-//  uint32_t delay = r % TDMA_HIGH_RES_RAND;
-//  transmitTime.full += delay;
+  // uint32_t r = random();
+  // uint32_t delay = r % TDMA_HIGH_RES_RAND;
+  // transmitTime.full += delay;
 
   // DW1000 can only schedule time with 9 LSB at 0, adjust for it
   adjustTxRxTime(&transmitTime);
@@ -169,49 +301,44 @@ static dwTime_t transmitTimeAsSoonAsPossible(dwDevice_t *dev)
   return transmitTime;
 }
 
-static void calculateDistance(int slot, int newId, int remotePid, uint32_t remoteTx, uint32_t remoteRx, uint32_t ts)
+
+static uint16_t calculateDistance(anchorContext_t* anchorCtx, int remoteTxSeqNr, int remoteRxSeqNr, uint32_t remoteTx, uint32_t remoteRx, uint32_t rx)
 {
-  // Check that the 2 last packets are consecutive packets and that our last packet is in beteen
-  if ((ctx.packetIds[slot] == ((newId-1) & 0x07f)) && remotePid == ctx.packetIds[ctx.anchorId]) {
-    double tround1 = remoteRx - ctx.txTimestamps[slot];
-    double treply1 = ctx.txTimestamps[ctx.anchorId] - ctx.rxTimestamps[slot];
-    double tround2 = ts - ctx.txTimestamps[ctx.anchorId];
+  // Check that the 2 last packets are consecutive packets and that our last packet is in between
+  if ((anchorCtx->seqNr == ((remoteTxSeqNr - 1) & 0x07f)) && remoteRxSeqNr == ctx.seqNr) {
+    double tround1 = remoteRx - anchorCtx->txTimeStamp;
+    double treply1 = ctx.txTime - anchorCtx->rxTimeStamp;
+    double tround2 = rx - ctx.txTime;
     double treply2 = remoteTx - remoteRx;
 
-    uint32_t distance = ((tround2 * tround1)-(treply1 * treply2)) / (2*(treply1 + tround2));
-    ctx.distances[slot] = distance & 0xfffful;
+    uint32_t distance = ((tround2 * tround1)-(treply1 * treply2)) / (2 * (treply1 + tround2));
+    return distance & 0xfffful;
   } else {
-    ctx.distances[slot] = 0;
+    return 0;
   }
 }
 
-static void convertTdoa3ToTdoa2(const uint8_t slot, const rangePacket3_t* range3, rangePacket2_t* range2)
-{
-  memset(range2, 0, sizeof(rangePacket2_t));
-
-  range2->pid[slot] = range3->header.seq;
-  memcpy(&range2->timestamps[slot], &range3->header.txTimeStamp, TS_TX_SIZE);
-
-  const void* anchorDataPtr = &range3->remoteAnchorData;
-  for (uint8_t i = 0; i < range3->header.remoteCount; i++) {
+static bool extractFromPacket(const rangePacket3_t* rangePacket, uint32_t* remoteRx, uint8_t* remoteRxSeqNr) {
+  const void* anchorDataPtr = &rangePacket->remoteAnchorData;
+  for (uint8_t i = 0; i < rangePacket->header.remoteCount; i++) {
     remoteAnchorDataFull_t* anchorData = (remoteAnchorDataFull_t*)anchorDataPtr;
 
-    uint8_t id = anchorData->id;
-    if (id < NSLOTS) {
-      uint8_t seq = anchorData->seq & 0x7f;
-      bool hasDistance = ((anchorData->seq & 0x80) != 0);
+    const uint8_t id = anchorData->id;
+    if (id == ctx.anchorId) {
+      *remoteRxSeqNr = anchorData->seq & 0x7f;
+      *remoteRx = anchorData->rxTimeStamp;
+      return true;
+    }
 
-      range2->pid[id] = seq;
-      memcpy(&range2->timestamps[id], &anchorData->rxTimeStamp, TS_TX_SIZE);
-
-      if (hasDistance) {
-        range2->distances[id] = anchorData->distance;
-        anchorDataPtr += sizeof(remoteAnchorDataFull_t);
-      } else {
-        anchorDataPtr += sizeof(remoteAnchorDataShort_t);
-      }
+    bool hasDistance = ((anchorData->seq & 0x80) != 0);
+    if (hasDistance) {
+      anchorDataPtr += sizeof(remoteAnchorDataFull_t);
+    } else {
+      anchorDataPtr += sizeof(remoteAnchorDataShort_t);
     }
   }
+
+  return false;
 }
 
 static void handleRxPacket(dwDevice_t *dev)
@@ -230,25 +357,30 @@ static void handleRxPacket(dwDevice_t *dev)
     return;
   }
 
-  const int slot = rxPacket.sourceAddress[0];
-  if (slot < NSLOTS) {
-    rangePacket3_t* rangePacket3 = (rangePacket3_t *)rxPacket.payload;
+  const uint8_t remoteAnchorId = rxPacket.sourceAddress[0];
+  ctx.anchorRxCount[remoteAnchorId]++;
+  anchorContext_t* anchorCtx = getContext(remoteAnchorId);
+  if (anchorCtx) {
+    const rangePacket3_t* rangePacket = (rangePacket3_t *)rxPacket.payload;
 
-    rangePacket2_t rangePacketMem;
-    rangePacket2_t* rangePacket = &rangePacketMem;
-    convertTdoa3ToTdoa2(slot, rangePacket3, rangePacket);
+    uint32_t remoteTx = rangePacket->header.txTimeStamp;
+    uint8_t remoteTxSeqNr = rangePacket->header.seq;
 
-    uint32_t remoteTx;
-    memcpy(&remoteTx, rangePacket->timestamps[slot], 4);
-    uint32_t remoteRx;
-    memcpy(&remoteRx, rangePacket->timestamps[ctx.anchorId], 4);
+    uint32_t remoteRx = 0;
+    uint8_t remoteRxSeqNr = 0;
+    bool dataFound = extractFromPacket(rangePacket, &remoteRx, &remoteRxSeqNr);
 
-    calculateDistance(slot, rangePacket->pid[slot], rangePacket->pid[ctx.anchorId],
-                      remoteTx, remoteRx, rxTime.low32);
+    if (dataFound) {
+      uint16_t distance = calculateDistance(anchorCtx, remoteTxSeqNr, remoteRxSeqNr, remoteTx, remoteRx, rxTime.low32);
 
-    ctx.packetIds[slot] = rangePacket->pid[slot];
-    ctx.rxTimestamps[slot] = rxTime.low32;
-    memcpy(&ctx.txTimestamps[slot], &rangePacket->timestamps[slot], 4);
+      if (distance > 0) {
+        anchorCtx->distance = distance;
+      }
+
+      anchorCtx->rxTimeStamp = rxTime.low32;
+      anchorCtx->seqNr = remoteTxSeqNr;
+      anchorCtx->txTimeStamp = remoteTx;
+    }
   }
 }
 
@@ -265,23 +397,24 @@ static void setupRx(dwDevice_t *dev)
 static int populateTxData(rangePacket3_t *rangePacket)
 {
   // rangePacket->header.type already populated
-  rangePacket->header.seq = ctx.packetIds[ctx.anchorId];
-  memcpy(&rangePacket->header.txTimeStamp, &ctx.txTimestamps[ctx.anchorId], TS_TX_SIZE);
-  rangePacket->header.remoteCount = NSLOTS - 1;
+  rangePacket->header.seq = ctx.seqNr;
+  rangePacket->header.txTimeStamp = ctx.txTime;
+  rangePacket->header.remoteCount = ctx.remoteTxIdCount;
 
   remoteAnchorDataFull_t* anchorData = (remoteAnchorDataFull_t*) &rangePacket->remoteAnchorData;
-  for (uint8_t i = 0; i < NSLOTS; i++) {
-    if (i != ctx.anchorId) {
-      anchorData->id = i;
-      anchorData->seq = ctx.packetIds[i] | 0x80;
-      memcpy(&anchorData->rxTimeStamp, &ctx.rxTimestamps[i], TS_TX_SIZE);
-      anchorData->distance = ctx.distances[i];
+  for (uint8_t i = 0; i < ctx.remoteTxIdCount; i++) {
+    uint8_t id = ctx.remoteTxId[i];
+    anchorContext_t* anchorCtx = getContext(id);
 
-      anchorData++;
-    }
+    anchorData->id = id;
+    anchorData->seq = anchorCtx->seqNr | 0x80;
+    anchorData->rxTimeStamp = anchorCtx->rxTimeStamp;
+    anchorData->distance = anchorCtx->distance;
+
+    anchorData++;
   }
 
-  return FULL_RANGE_PACKET_SIZE;
+  return (void*)anchorData - (void*)rangePacket;
 }
 
 // Set TX data in the radio TX buffer
@@ -309,11 +442,11 @@ static void setTxData(dwDevice_t *dev)
 
   // LPP anchor position is currently sent in all packets
   if (uwbConfig->positionEnabled) {
-    txPacket.payload[LPP_HEADER] = SHORT_LPP;
-    txPacket.payload[LPP_TYPE] = LPP_SHORT_ANCHOR_POSITION;
+    txPacket.payload[rangePacketSize + LPP_HEADER] = SHORT_LPP;
+    txPacket.payload[rangePacketSize + LPP_TYPE] = LPP_SHORT_ANCHOR_POSITION;
 
-    struct lppShortAnchorPosition_s *pos = (struct lppShortAnchorPosition_s*) &txPacket.payload[LPP_PAYLOAD];
-    memcpy(pos->position, uwbConfig->position, 3*sizeof(float));
+    struct lppShortAnchorPosition_s *pos = (struct lppShortAnchorPosition_s*) &txPacket.payload[rangePacketSize + LPP_PAYLOAD];
+    memcpy(pos->position, uwbConfig->position, 3 * sizeof(float));
 
     lppLength = 2 + sizeof(struct lppShortAnchorPosition_s);
   }
@@ -324,15 +457,14 @@ static void setTxData(dwDevice_t *dev)
 // Setup the radio to send a packet
 static void setupTx(dwDevice_t *dev)
 {
-  ctx.packetIds[ctx.anchorId] = ctx.pid;
-  ctx.pid = (ctx.pid + 1) & 0x7f;
+  dwTime_t txTime = findTransmitTimeAsSoonAsPossible(dev);
+  ctx.txTime = txTime.low32;
+  ctx.seqNr = (ctx.seqNr + 1) & 0x7f;
 
-  dwTime_t txTime = transmitTimeAsSoonAsPossible(dev);
-  ctx.txTimestamps[ctx.anchorId] = txTime.low32;
+  setTxData(dev);
 
   dwNewTransmit(dev);
   dwSetDefaults(dev);
-  setTxData(dev);
   dwSetTxRxTime(dev, txTime);
 
   dwStartTransmit(dev);
@@ -371,8 +503,18 @@ static uint32_t startNextEvent(dwDevice_t *dev)
 static void tdoa3Init(uwbConfig_t * config, dwDevice_t *dev)
 {
   ctx.anchorId = config->address[0];
-  memset(ctx.txTimestamps, 0, sizeof(ctx.txTimestamps));
-  memset(ctx.rxTimestamps, 0, sizeof(ctx.rxTimestamps));
+  ctx.seqNr = 0;
+  ctx.txTime = 0;
+  ctx.nextTxTick = 0;
+  ctx.remoteTxIdCount = 0;
+  ctx.nextAnchorListUpdate = 0;
+
+  memset(&ctx.anchorCtxLookup, ID_WITHOUT_CONTEXT, ID_COUNT);
+  for (int i = 0; i < ANCHOR_STORAGE_COUNT; i++) {
+    ctx.anchorCtx[i].isUsed = false;
+  }
+
+  clearAnchorRxCount();
 }
 
 // Called for each DW radio event
@@ -386,6 +528,12 @@ static uint32_t tdoa3UwbEvent(dwDevice_t *dev, uwbEvent_t event)
     default:
       // Nothing here
       break;
+  }
+
+  uint32_t now = xTaskGetTickCount();
+  if (now > ctx.nextAnchorListUpdate) {
+    updateAnchorLists();
+    ctx.nextAnchorListUpdate = now + ANCHOR_LIST_UPDATE_INTERVAL;
   }
 
   uint32_t timeout_ms = startNextEvent(dev);
