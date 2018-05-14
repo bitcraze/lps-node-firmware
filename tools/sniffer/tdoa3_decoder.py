525690bd7c438121f4044d77f5ece968c27925a1
Kristoffer Richardsson <kristoffer@bitcraze.io>
2018-05-14 06:19:14 +0200

    #27 Updated TDoA3 sniffer with filter and added tool to extract anchor distances

--- tools/sniffer/tdoa3_decoder.py
+++ tools/sniffer/tdoa3_decoder.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# YAML sniffer log TDOA2 decoder, generates a stream of YAML documents
+# YAML sniffer log TDOA3 decoder, generates a stream of YAML documents
 # containing the decoded packet
 # out of simplicity (and lazyness), reads from STDIN and write to STDOUT,
 # just use your shell piping functionalities!
@@ -7,6 +7,19 @@
 import sys
 import yaml
 import struct
+
+id_filter = None
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        if arg.isdigit():
+            if not id_filter:
+                id_filter = []
+            id_filter.append(int(arg))
+        else:
+            print("usage: {} [id filter]".format(sys.argv[0]))
+            print("  id filter: a list of anchor ids. Default is to let all pass")
+            sys.exit(1)
+
 
 # Packet format:
 # typedef struct {
@@ -33,65 +46,68 @@ for packet in yaml.load_all(sys.stdin, Loader=yaml.CLoader):
     if not packet:
         continue
 
-    # Its alost free so we keel the raw data and just add the decoded fields
-    packetType = packet["data"][0]
-    packet["type"] = packetType
+    id = packet['from']
+    if not id_filter or id in id_filter:
 
-    if packetType == 0x30:
-        decoded = struct.unpack("<BBLB", packet["data"][:7])
-        packet["seq"] = decoded[1]
-        packet["txTimeStamp"] = decoded[2]
+        # Its alost free so we keel the raw data and just add the decoded fields
+        packetType = packet["data"][0]
+        packet["type"] = packetType
 
-        remote_count = decoded[3]
-        packet["remoteCount"] = remote_count
+        if packetType == 0x30:
+            decoded = struct.unpack("<BBLB", packet["data"][:7])
+            packet["seq"] = decoded[1]
+            packet["txTimeStamp"] = decoded[2]
 
-        packet['remoteAnchorData'] = []
-        anchor_data_index = 7
+            remote_count = decoded[3]
+            packet["remoteCount"] = remote_count
 
-        for i in range(remote_count):
-            decoded_anchor_data = struct.unpack(
-                '<BBL',
-                packet["data"][anchor_data_index:(anchor_data_index + 6)])
-            seq = decoded_anchor_data[1] & 0x7f
-            anchor_data = {
-                'id': decoded_anchor_data[0],
-                'seq': seq,
-                'rxTimeStamp': decoded_anchor_data[2],
-            }
-            anchor_data_index += 6
+            packet['remoteAnchorData'] = []
+            anchor_data_index = 7
 
-            has_distance = ((decoded_anchor_data[1] & 0x80) != 0)
-            if (has_distance):
-                decoded_distance = struct.unpack(
-                    '<H',
-                    packet["data"][anchor_data_index:(anchor_data_index + 2)])
-                anchor_data['distance'] = decoded_distance[0]
-                anchor_data_index += 2
+            for i in range(remote_count):
+                decoded_anchor_data = struct.unpack(
+                    '<BBL',
+                    packet["data"][anchor_data_index:(anchor_data_index + 6)])
+                seq = decoded_anchor_data[1] & 0x7f
+                anchor_data = {
+                    'id': decoded_anchor_data[0],
+                    'seq': seq,
+                    'rxTimeStamp': decoded_anchor_data[2],
+                }
+                anchor_data_index += 6
 
-            packet['remoteAnchorData'].append(anchor_data)
+                has_distance = ((decoded_anchor_data[1] & 0x80) != 0)
+                if (has_distance):
+                    decoded_distance = struct.unpack(
+                        '<H',
+                        packet["data"][anchor_data_index:(anchor_data_index + 2)])
+                    anchor_data['distance'] = decoded_distance[0]
+                    anchor_data_index += 2
 
-        if len(packet["data"]) > anchor_data_index:
-            if packet["data"][anchor_data_index] == 0xf0 and \
-                    packet["data"][anchor_data_index + 1] == 0x01:
-                packet["lpp_data"] = {}
-                packet["lpp_data"]['header'] = packet["data"][
-                    anchor_data_index]
-                anchor_data_index += 1
+                packet['remoteAnchorData'].append(anchor_data)
 
-                packet["lpp_data"]['type'] = packet["data"][anchor_data_index]
-                anchor_data_index += 1
+            if len(packet["data"]) > anchor_data_index:
+                if packet["data"][anchor_data_index] == 0xf0 and \
+                        packet["data"][anchor_data_index + 1] == 0x01:
+                    packet["lpp_data"] = {}
+                    packet["lpp_data"]['header'] = packet["data"][
+                        anchor_data_index]
+                    anchor_data_index += 1
 
-                decoded = struct.unpack(
-                    "<fff",
-                    packet["data"][anchor_data_index:anchor_data_index + 3 * 6]
-                )
-                packet["lpp_data"]['position'] = {}
-                packet["lpp_data"]['position']['x'] = decoded[0]
-                packet["lpp_data"]['position']['y'] = decoded[1]
-                packet["lpp_data"]['position']['z'] = decoded[2]
-                anchor_data_index += 3 * 6
-            else:
-                packet["lpp_data"] = packet["data"][anchor_data_index:]
+                    packet["lpp_data"]['type'] = packet["data"][anchor_data_index]
+                    anchor_data_index += 1
 
-    print("---")
-    print(yaml.dump(packet, Dumper=yaml.CDumper))
+                    decoded = struct.unpack(
+                        "<fff",
+                        packet["data"][anchor_data_index:anchor_data_index + 3 * 6]
+                    )
+                    packet["lpp_data"]['position'] = {}
+                    packet["lpp_data"]['position']['x'] = decoded[0]
+                    packet["lpp_data"]['position']['y'] = decoded[1]
+                    packet["lpp_data"]['position']['z'] = decoded[2]
+                    anchor_data_index += 3 * 6
+                else:
+                    packet["lpp_data"] = packet["data"][anchor_data_index:]
+
+        print("---")
+        print(yaml.dump(packet, Dumper=yaml.CDumper))
---
+++ tools/sniffer/tdoa3_tof.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# YAML sniffer that extracts tof data from tdoa3 parsed yaml documents.
+# out of simplicity (and lazyness), reads from STDIN and write to STDOUT,
+# just use your shell piping functionalities!
+# sniffer_binary -> tdoa3_decoder -> tdoa3_tof
+
+import sys
+import yaml
+import struct
+
+unit = 'ticks'
+
+ANTENNA_OFFSET = 154.6
+LOCODECK_TS_FREQ = 499.2e6 * 128
+SPEED_OF_LIGHT = 299792458.0
+M_PER_TICK = SPEED_OF_LIGHT / LOCODECK_TS_FREQ
+
+if len(sys.argv) > 1:
+    for arg in sys.argv[1:]:
+        if arg == 'm':
+            unit = 'meters'
+        elif arg == 't':
+            unit = 'ticks'
+        else:
+            print("usage: {} [unit]".format(sys.argv[0]))
+            print("  unit: t = ticks (default), m = meters (excluding antenna delay)")
+            sys.exit(1)
+
+
+for packet in yaml.load_all(sys.stdin, Loader=yaml.CLoader):
+    if not packet:
+        continue
+
+    data = {'id': packet['from'], 'tof': {}}
+
+    for remote in packet['remoteAnchorData']:
+        tof = remote['distance']
+        remote_id = remote['id']
+        if unit == 'ticks':
+            data['tof'][remote_id] = tof
+        if unit == 'meters':
+            data['tof'][remote_id] = tof * M_PER_TICK - ANTENNA_OFFSET
+
+    print("---")
+    print(yaml.dump(data, Dumper=yaml.CDumper))
